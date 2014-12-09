be424af09057509673da7567c4ce3951272a07f0
Janyell <e.sycheva_nn@mail.ru>
2014-12-09 16:39:05 +0300

    hash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+import argparse
+
 __author__ = 'popka'
 
 from PageClass.Page import Page
@@ -27,8 +29,14 @@ def try_one_page(driver, url):
     return pageo.try_page()
 
 
-if __name__ == '__main__':
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--login', action='store_const', const=True, default=False)
+    parser.add_argument('url')
+    parser.add_argument('-o', '--one', action='store_const', const=True, default=False)
+    return parser
 
+if __name__ == '__main__':
 
     """
     Чтобы не включать selenuim сервер руками. Через раз работает. ХЗ почему.
@@ -37,60 +45,58 @@ if __name__ == '__main__':
     time.sleep(2)
     """
 
-    MAIN_URL = "https://xss-doc.appspot.com/demo/2" # Считывать, как аргумент командной строки
+    parser = create_parser()
+    namespace = parser.parse_args()
+
+    MAIN_URL = namespace.url # Считывать, как аргумент командной строки
 
     driver = Remote(
             command_executor='http://127.0.0.1:4444/wd/hub',
             desired_capabilities=getattr(DesiredCapabilities, browser).copy()
     )
 
-
-    '''
-    auth ВВОД ВРЕМЕНИ, Отводимого на авторизацию
-    page = Page(driver=driver, url=MAIN_URL)
-    page.open()
-    time.sleep(20)
-    '''
-
-    #'''
-    #КАРТА САЙТА. Нужна, если чекаем все урлы на страничке (обычныый случай)
-
-    link_container = LinkContainer.LinkContainer()
-    link_container.add([MAIN_URL])
-
-    urls_with_parameters = []
-    i = 0
-    print('Generating map of site')
-
-    while ((i < link_container.get_length()) and (i < MAX_PAGES)):
-        url = link_container.get_link(i)
-        print('.')
-
-        page = Page(driver=driver, url=url)
+    # auth ВВОД ВРЕМЕНИ, Отводимого на авторизацию
+    if namespace.login:
+        page = Page(driver=driver, url=MAIN_URL)
         page.open()
+        time.sleep(20)
 
-        link_container.add(page.get_inner_links())
+    if namespace.one is False:
+        #'''
+        #КАРТА САЙТА. Нужна, если чекаем все урлы на страничке (обычный случай)
 
-        urls_with_parameters += page.try_page()
+        link_container = LinkContainer.LinkContainer()
+        link_container.add([MAIN_URL])
 
-        i += 1
-        time.sleep(0.5)
+        urls_with_parameters = []
+        i = 0
+        print('Generating map of site')
 
-    urls_with_parameters = set(urls_with_parameters) #Сделать список уникальным
+        while ((i < link_container.get_length()) and (i < MAX_PAGES)):
+            url = link_container.get_link(i)
+            print('.')
 
-    print(link_container.get_all_links())
-    print(urls_with_parameters)
+            page = Page(driver=driver, url=url)
+            page.open()
 
-    #''' Карта сайта
+            link_container.add(page.get_inner_links())
 
-    urls_with_parameters = set()
+            urls_with_parameters += page.try_page()
 
-    '''
+            i += 1
+            time.sleep(0.5)
 
-    if (True): # ЕСЛИ ОДИН УРЛ ВКЛЮЧАЕМ ЭТО ВЕТВЛЕНИЕ ВМЕСТО ВЕРХНЕГО
+        urls_with_parameters = set(urls_with_parameters) #Сделать список уникальным
+
+        print(link_container.get_all_links())
+        print(urls_with_parameters)
+
+        #''' Карта сайта
+
+        urls_with_parameters = set()
+    else:
+        # ЕСЛИ ОДИН УРЛ ВКЛЮЧАЕМ ЭТО ВЕТВЛЕНИЕ ВМЕСТО ВЕРХНЕГО
         urls_with_parameters = set(try_one_page(driver=driver, url=MAIN_URL))
-
-    '''
 
     print(urls_with_parameters)
     xss_checker = XssChecker(driver)
